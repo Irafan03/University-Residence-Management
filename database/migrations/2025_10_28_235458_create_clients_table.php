ec37f32cbf71ef5419275d45358766418d6d0ada
Irafan Imane <Irafanimane@gmail.com>
2025-10-29 01:47:55 +0100

    Initail commit

---
+++ database/migrations/2025_10_28_235458_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->string('Telephone');
+            $table->string('CIN')->unique();
+            $table->string('attestation')->nullable();
+            $table->date('date_naissance');
+            $table->string('niveau_scolaire');
+            $table->string('universite');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
